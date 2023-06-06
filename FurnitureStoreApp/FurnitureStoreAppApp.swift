8f2e4d3ecb27abfb999c06dc698993c4bece07b9
Aleksey <oldf03@gmail.com>
2023-06-06 09:57:13 +0300

    Initial Commit

---
+++ FurnitureStoreApp/FurnitureStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureStoreAppApp.swift
+//  FurnitureStoreApp
+//
+//  Created by Buba on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
